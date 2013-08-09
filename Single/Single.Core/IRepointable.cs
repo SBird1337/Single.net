d8333b277975c51a296ee934582f70ade4b225f0
SBird1337 <auerphilipp38@yahoo.de>
2013-08-09 12:16:20 +0200

    Initial Project Commit
    Adding All of the project files

---
+++ Single/Single.Core/IRepointable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Single.Core
+{
+    public interface IRepointable : IRomWritable
+    {
+        int GetSize();
+        UInt32 GetCurrentOffset();
+        void SetCurrentOffset(UInt32 Offset);
+    }
+}
